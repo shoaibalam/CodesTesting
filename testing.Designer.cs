c1f7d6ebf60f5c4eae1fa8165486cb6d1bbd0dbf
shoaibalam <shoaib.al9@gmail.com>
2018-03-27 16:57:42 +0530

    test2

--- testing.Designer.cs
+++ testing.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // testing
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(837, 385);
+            this.Name = "testing";
             this.Text = "testing";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
