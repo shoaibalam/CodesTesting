40d8e5b054a9967514f8b094bd6eb1c5386cc720
shoaibalam <shoaib.al9@gmail.com>
2018-03-27 16:51:23 +0530

    testdsfsdfsf
    sdfdasfasf

--- MDIParent.cs
+++ MDIParent.cs
@@ -140,5 +140,10 @@ namespace MANUUFinance
             frmDemonstration objFrmDemonstration = new frmDemonstration();
             objFrmDemonstration.ShowDialog();
         }
+
+        private void againToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
